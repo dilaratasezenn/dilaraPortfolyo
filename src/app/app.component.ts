4ca3fe784ec3898f187e31d9ef5b04395a1db178
dilaratasezenn <dilaratasezenn@gmail.com>
2024-04-24 13:30:17 +0300

    merhaba yazısına animasyon eklendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dilara Taşezen';
+  public animatedHeading='';
   public animatedName = '';
   public animatedDescription = '';
-
+private heading ='Merhaba,';
   private name = 'ben Dilara Taşezen';
-  private description = 'Web Programlama alanında kendimi geliştirmekteyim.';
+  private description = 'web geliştirme alanında kendimi geliştirmekteyim.';
   private index = 0;
-  private delay = 150;
+  private delay = 100;
 
   constructor() {
     this.animateText();
   }
 animateText() {
-    if (this.index < this.name.length) {
-      this.animatedName += this.name.charAt(this.index);
+    if (this.index < this.heading.length) {
+      this.animatedHeading += this.heading.charAt(this.index);
       this.index++;
       setTimeout(() => this.animateText(), this.delay);
-    } else if (this.index - this.name.length < this.description.length) {
-      this.animatedDescription += this.description.charAt(this.index - this.name.length);
+    } 
+    else if(this.index - this.heading.length < this.name.length)
+      {
+        this.animatedName += this.name.charAt(this.index - this.heading.length);
+        this.index++;
+        setTimeout(() => this.animateText(), this.delay);
+      }
+    
+    
+    else if (this.index - this.name.length - this.heading.length < this.description.length) {
+      this.animatedDescription += this.description.charAt(this.index - this.name.length - this.heading.length);
       this.index++;
       setTimeout(() => this.animateText(), this.delay);
     }
