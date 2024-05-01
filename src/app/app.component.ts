b44446d2d9d0219e73f91afd0b6621173cae5809
dilaratasezenn <dilaratasezenn@gmail.com>
2024-05-01 23:17:21 +0300

    gereksiz dosyalar yorum satırları silindi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  flipInYAnimation,
-  flipInYOnEnterAnimation,
-} from './animations/flipinY';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  animations: [flipInYAnimation(), flipInYOnEnterAnimation()],
 })
 export class AppComponent implements OnInit {
 
@@ -60,69 +56,3 @@ export class AppComponent implements OnInit {
     }
   }
 }
-/* progressHtmlValue: number = 0;
-  progressCssValue: number = 0;
-  progressBootstrapValue: number = 0;
-  progressAngularValue: number = 0;
-  progressAspValue: number = 0;*/
-
-    /*this.skillsNumberIncrease('html',100);
-    this.skillsNumberIncrease('css',90);
-    this.skillsNumberIncrease('bootstrap',70);
-    this.skillsNumberIncrease('angular',50);
-    this.skillsNumberIncrease('asp',25);
-  }
-  skillsNumberIncrease(skill: string, level: number) {
-    let i = 0;
-    switch (skill) {
-      case 'html':
-      let intervalHtml = setInterval(() => {
-        if (i <= level) {
-          this.progressHtmlValue = i;
-          i++;
-        } else {
-          clearInterval(intervalHtml);
-        }
-      }, 25);
-        break;
-      case 'css':
-        let intervalCss = setInterval(() => {
-          if (i <= level) {
-            this.progressCssValue = i;
-            i++;
-          } else {
-            clearInterval(intervalCss);
-          }
-        }, 25);
-        break;
-      case 'bootstrap':
-        let intervalBootstrap = setInterval(() => {
-          if (i <= level) {
-            this.progressBootstrapValue = i;
-            i++;
-          } else {
-            clearInterval(intervalBootstrap);
-          }
-        }, 25);
-        break;
-      case 'angular':
-        let intervalAngular = setInterval(() => {
-          if (i <= level) {
-            this.progressAngularValue = i;
-            i++;
-          } else {
-            clearInterval(intervalAngular);
-          }
-        }, 25);
-        break;
-      case 'asp':
-        let intervalAsp = setInterval(() => {
-          if (i <= level) {
-            this.progressAspValue = i;
-            i++;
-          } else {
-            clearInterval(intervalAsp);
-          }
-        }, 25);
-          break;
-    }*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { EducationComponent } from './education/education.component';
+
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EducationComponent,
+    
     FooterComponent
   ],
   imports: [
